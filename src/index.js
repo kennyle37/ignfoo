5625f969a230523936f4c1100a2e9af07598bfa5
Kenny Le <kennyle37@gmail.com>
2019-02-26 22:27:33 -0800

    top-header

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 
 import './sass/index.scss';
 
+import TopHeaderContainer from './top-header/TopHeaderContainer';
+import SideNavContainer from './side-nav/SideNavContainer';
+import SideSectionContainer from './side-section/SideSectionContainer';
+
 class App extends Component {
   render() {
     return (
-      <div>Hello</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-sm-8 col-md-6 col-lg-12">
+            <TopHeaderContainer title="Latest News" />
+          </div>
+        </div>
+      </div>
     )
   }
 }
