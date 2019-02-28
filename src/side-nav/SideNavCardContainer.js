dd024dc9a93691849d04cc52808205c48dbe6d31
Kenny Le <kennyle37@gmail.com>
2019-02-28 13:02:03 -0800

    added selection option and cursor pointers

--- src/side-nav/SideNavCardContainer.js
+++ src/side-nav/SideNavCardContainer.js
@@ -1,30 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SideNavCardContainer = () => {
-  return (
-    <div className="row SideNav--card-container">
-        <li className="col-md-12 SideNav--card active">
-          <span className="col-md-3 col-xs-6 SideNav--card-logo">
-            <i className="fas fa-clock" />
-          </span>
-          <span className="col-md-6 col-xs-6 SideNav--card-description">Latest</span>
-        </li>
-        
-        <li className="col-md-12 SideNav--card ">
-          <span className="col-md-3 col-xs-6 SideNav--card-logo">
-            <i className="fas fa-play" />
-          </span>
-          <span className="col-md-6 col-xs-6 SideNav--card-description">Videos</span>
-        </li>
+class SideNavCardContainer extends Component {
+  state = {
+    selected: false,
+  }
 
-        <li className="col-md-12 SideNav--card ">
-          <span className="col-md-3 col-xs-6 SideNav--card-logo">
-            <i className="fas fa-file-alt" />
-          </span>
-          <span className="col-md-6 col-xs-6 SideNav--card-description">Articles</span>
-        </li>
-    </div>
-  )
+  onNavSelection = (e) => {
+    document.querySelector('.active').classList.remove('active');
+    e.currentTarget.classList.add('active');
+  }
+
+  render() {
+    const { selected } = this.state;
+    return (
+      <div className="row SideNav--card-container">
+          <li className="col-md-12 SideNav--card active" onClick={this.onNavSelection}>
+            <span className="col-md-3 col-xs-6 SideNav--card-logo">
+              <i className="fas fa-clock" />
+            </span>
+            <span className="col-md-6 col-xs-6 SideNav--card-description">Latest</span>
+          </li>
+          
+          <li className="col-md-12 SideNav--card" onClick={this.onNavSelection}>
+            <span className="col-md-3 col-xs-6 SideNav--card-logo">
+              <i className="fas fa-play" />
+            </span>
+            <span className="col-md-6 col-xs-6 SideNav--card-description">Videos</span>
+          </li>
+  
+          <li className="col-md-12 SideNav--card" onClick={this.onNavSelection}>
+            <span className="col-md-3 col-xs-6 SideNav--card-logo">
+              <i className="fas fa-file-alt" />
+            </span>
+            <span className="col-md-6 col-xs-6 SideNav--card-description">Articles</span>
+          </li>
+      </div>
+    )
+  }
 }
+
+  
+
+
 
 export default SideNavCardContainer;
